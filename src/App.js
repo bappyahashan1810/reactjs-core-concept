fed3cec98b59be9a77c604854e7b2d95327ea005
bappy-ahashan <bappyahashan@gmail.com>
2023-07-07 00:34:48 +0600

    something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <Counter></Counter>
+      }
+    </div>
+  );
+}
+function Counter() {
+  const [count, countState] = useState(55);
+  function Increase() { countState(count + 1) };
+  function Decrease() { countState(count - 1) };
+  return (
+    <div>
+      <h1>Counter:{count}</h1>
+      <button onClick={Increase}>Increase</button>
+      <button onClick={Decrease}>Decrese</button>
+    </div>
+  );
+}
+
+
+function Product(props) {
+  return (
+    <div className='product'>
+      <h4>Product_name:{props.name}</h4>
+      <p>Product_Price:{props.price}</p>
     </div>
   );
 }
